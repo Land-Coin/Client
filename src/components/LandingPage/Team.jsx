c2474592f088c5dad81aabb43340efa39e18e4e1
AlexMuia31 <alexmuia31@gmail.com>
2022-09-06 16:11:37 +0300

    team start

--- src/components/LandingPage/Team.jsx
+++ src/components/LandingPage/Team.jsx
@@ -1,4 +1,4 @@
-import { Box, Container } from "@mui/material";
+import { Box, Container, Typography } from "@mui/material";
 import React from "react";
 
 const Team = () => {
@@ -8,7 +8,20 @@ const Team = () => {
         background: "linear-gradient(106.42deg, #E2E7E6 -0.82%, #ECE5E6 100%)",
       }}
     >
-      <Container maxWidth="xl"></Container>
+      <Container maxWidth="xl">
+        <Box sx={{ display: "flex", justifyContent: "center", pt: "5%" }}>
+          <Typography
+            sx={{
+              color: "#1C1B2D",
+              fontSize: "52px",
+              fontWeight: 700,
+              lineHeight: "63px",
+            }}
+          >
+            Our Team
+          </Typography>
+        </Box>
+      </Container>
     </Box>
   );
 };
--- src/components/LandingPage/index.jsx
+++ src/components/LandingPage/index.jsx
@@ -2,12 +2,14 @@ import { Box, Container, Toolbar } from "@mui/material";
 import React from "react";
 import Banner from "./Banner";
 import Services from "./Services";
+import Team from "./Team";
 
 const LandingPage = () => {
   return (
     <Box>
       <Banner />
       <Services />
+      <Team />
     </Box>
   );
 };
